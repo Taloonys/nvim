3344a10fb303159e9e148e25bb894844cfae615d
Taloonys <vadim-dev@vadimrevi11pc.>
2025-08-04 16:02:25 +0300

    feat: tabnine smart completion

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -23,6 +23,23 @@ lspconfig.dockerls.setup({ capabilites = capabilities })
 -- ╔══════════════════════════════╗
 -- ║    Autocompletion sources    ║
 -- ╚══════════════════════════════╝
+local tabnine = require("cmp_tabnine.config")
+
+tabnine:setup({
+	max_lines = 1000,
+	max_num_results = 20,
+	sort = true,
+	run_on_every_keystroke = true,
+	snippet_placeholder = "..",
+	ignored_file_types = {
+		-- default is not to ignore
+		-- uncomment to ignore in lua:
+		lua = true,
+	},
+	show_prediction_strength = true,
+	min_percent = 0,
+})
+
 local cmp = require("cmp")
 cmp.setup({
 	snippet = {
@@ -34,39 +51,38 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		-- ['<C-e>'] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
+	mapping = {
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- <CR> = Enter. Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-Space>"] = cmp.mapping.complete(), -- Open completion list
+	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" }, -- lsp completion
 		{ name = "luasnip" }, -- For luasnip users.
-	}, {
 		{ name = "buffer" }, -- completion with words, that were used in current buffer
-	}, {
 		{ name = "path" }, -- system paths
-	}, {
-		name = "nvim_lsp_signature_help", -- show signature docs
+		{ name = "nvim_lsp_signature_help" }, -- show signature docs
+		{ name = "cmp_tabnine" },
+		{ name = "treesitter" },
 	}),
 })
 
 cmp.setup.cmdline("/", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = "buffer" }
-  }
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" }
-  }, {
-    { name = "cmdline" }
-  })
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 -- ╔══════════════════════╗
--- lua/plugins/completion-plugins.lua
+++ lua/plugins/completion-plugins.lua
@@ -1,13 +1,14 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"saadparwaiz1/cmp_luasnip",
 		"L3MON4D3/LuaSnip",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-cmdline",
 		"hrsh7th/cmp-nvim-lsp",
+		"ray-x/cmp-treesitter",
+		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
 		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-buffer",
 	},
 }
