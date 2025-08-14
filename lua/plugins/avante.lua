4c04afdd309f058636725c3bdd234d6ab8c7504d
Taloonys <vadim-dev@vadimrevi11pc.>
2025-08-14 23:35:13 +0300

    chore(ai): well, it could be avante...

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,66 @@
+return {
+	{
+		"yetone/avante.nvim",
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		-- ⚠️ must add this setting! ! !
+		build = vim.fn.has("win32") ~= 0
+				and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+			or "make",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		---@module 'avante'
+		---@type avante.Config
+		opts = {
+			provider = "ollama",
+			providers = {
+				ollama = {
+					endpoint = "http://localhost:11434",
+					model = "qwen2.5-coder:7b",
+					diable_tools = true, -- currently `replace_edit_in_file`
+					-- system_prompt = [[
+					--                    You are a code editing assistant inside Neovim.
+					--                    When asked to edit a file, you MUST return ONLY in unified diff format starting with --- and +++.
+					--                    Do not include explanations.
+					--                    ]],
+				},
+			},
+			behaviour = {
+				enable_cursor_planning_mode = true,
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			-- "stevearc/dressing.nvim", -- for input provider dressing
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
+}
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,27 +1,28 @@
-local opt              = vim.opt -- aka options
+local opt = vim.opt -- aka options
 
 -- ru-en layouts transfer
-vim.g.translate_source = 'ru'
-vim.g.translate_target = 'en'
+vim.g.translate_source = "ru"
+vim.g.translate_target = "en"
 
 -- ui enhacements
-opt.termguicolors      = true -- 24-bit rgb color support
+opt.termguicolors = true -- 24-bit rgb color support
 
 -- lines
-opt.number             = true
-opt.relativenumber     = true
-opt.cursorline         = true
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
 
 -- tabulation
-opt.shiftwidth         = 4    -- shift left 4 spaces on tab press
-opt.tabstop            = 4    -- 1 tab = 4 spaces
-opt.expandtab          = true -- tab is made by spaces
-opt.smartindent        = true -- new line auto tabbed
+opt.shiftwidth = 4 -- shift left 4 spaces on tab press
+opt.tabstop = 4 -- 1 tab = 4 spaces
+opt.expandtab = true -- tab is made by spaces
+opt.smartindent = true -- new line auto tabbed
 
 -- splits
-opt.splitright         = true -- vertical split вправо
-opt.splitbelow         = true -- horizontal split вниз
+opt.splitright = true -- vertical split вправо
+opt.splitbelow = true -- horizontal split вниз
 
 -- miscellaneous
-opt.mouse              = "a" -- allow mouse input
-opt.showmode           = false
+opt.mouse = "a" -- allow mouse input
+opt.showmode = false
+opt.laststatus = 3
