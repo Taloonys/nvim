6819ef13ef5d2e15bc64f78c48ac645c1840db89
v.makrousov <v.makrousov@securitycode.ru>
2025-08-29 13:10:11 +0300

    chore: some plugins renames 1

--- lua/plugins/markdown-table-mode.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'Kicamon/markdown-table-mode.nvim',
-  config = function()
-    require('markdown-table-mode').setup()
-    -- call `:Mtm` to ensert this mode, it will attempt to trigget lines with `|`
-  end
-}
