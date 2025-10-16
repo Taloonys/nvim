925164c0b121b27d327369c4eb8bffda2ee5e90f
Taloonys <vxcvxcv200071@gmail.com>
2025-10-16 23:00:30 +0300

    chore: cleaned for windows

--- init.lua
+++ init.lua
@@ -4,10 +4,3 @@ require("keymap")
 
 -- Plugin Manager
 require("config.lazy")
-
--- Afterwork
-require("after.lsp")
-require("after.colors")
-
--- Specific
-require("after.cpp-landscape.cmake") -- C++, cmake
--- lua/plugins/jobs.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<leader>jr", ":OverseerRun<CR>")
-vim.keymap.set("n", "<leader>jv", ":OverseerToggle<CR>")
-
-return {
-	"stevearc/overseer.nvim",
-	config = function()
-		require("overseer").setup()
-	end,
-}
