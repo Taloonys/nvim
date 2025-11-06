c94839b260b9ce8f8ae484f718080161edfc6092
Taloonys <vxcvxcv200071@gmail.com>
2025-11-07 01:29:45 +0300

    feat(motions): flash text jumps

--- lua/plugins/motions.lua
+++ lua/plugins/motions.lua
@@ -1,4 +1,55 @@
+local screen_jumps = {
+	-- Allows jumping like in helix and/or vimium(browser)
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+		{
+			"<c-s>",
+			mode = { "c" },
+			function()
+				require("flash").toggle()
+			end,
+			desc = "Toggle Flash Search",
+		},
+	},
+}
+
 local parser_based_motions = {
+	-- Allows parser-node-based moves
 	"ziontee113/syntax-tree-surfer",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
@@ -48,4 +99,4 @@ local parser_based_motions = {
 	end,
 }
 
-return { parser_based_motions }
+return { screen_jumps, parser_based_motions }
