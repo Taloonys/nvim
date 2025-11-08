3dc707578385599b2fbdc4992a0908b368a8a836
Taloonys <vxcvxcv200071@gmail.com>
2025-11-08 22:51:20 +0300

    feat(motions + edit): tabout + str joiner

--- lua/plugins/text-edit.lua
+++ lua/plugins/text-edit.lua
@@ -150,6 +150,16 @@ local pretty_fold = {
 	end,
 }
 
+local split_or_join_code = {
+	"Wansmer/treesj",
+	keys = { "<space>m", "<space>j", "<space>s" },
+	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+	config = function()
+		require("treesj").setup({--[[ your config ]]
+		})
+	end,
+}
+
 return {
 	surround,
 	autopairs,
@@ -157,4 +167,5 @@ return {
 	parser_based_refactor,
 	smart_substitution,
 	multi_cursor,
+	split_or_join_code,
 }
