5c0cdada69f4e6d694a9e7b92151b0ca5abb2b56
v.makrousov <v.makrousov@securitycode.ru>
2025-12-19 14:32:35 +0300

    feat: added comments collapse plugin

--- lua/plugins/code-documentation.lua
+++ lua/plugins/code-documentation.lua
@@ -1,4 +1,4 @@
-return {
+local docs_comments = {
 	"danymat/neogen",
 	-- config = true, -- default
 	config = function()
@@ -9,3 +9,28 @@ return {
 	-- Uncomment next line if you want to follow only stable versions
 	-- version = "*"
 }
+
+local fold_comments = {
+	"soemre/commentless.nvim",
+	cmd = "Commentless",
+	keys = {
+		{
+			"<leader>/",
+			function()
+				require("commentless").toggle()
+			end,
+			desc = "Toggle Comments",
+		},
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		-- Customize Configuration
+	},
+}
+
+-- vim.opt.foldminlines = 0 -- Allow folding/hiding single lines
+-- vim.opt.fillchars = "fold: " -- Remove the trailing dots but it will spawn `~` in empty strings
+
+return { docs_comments, fold_comments }
