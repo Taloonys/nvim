fff8bab3bf9df9d48c61e7005c3b676cd6a18bdb
v.makrousov <v.makrousov@securitycode.ru>
2026-04-27 14:19:47 +0300

    feat(mini): files keyset expansion

--- lua/plugins/mini-pack.lua
+++ lua/plugins/mini-pack.lua
@@ -15,6 +15,10 @@ local mini = {
 
 		require("mini.comment").setup() -- comment code
 
+		-------------------
+		-- File explorer --
+		-------------------
+
 		require("mini.files").setup({
 			-- oil-like files
 			windows = {
@@ -25,6 +29,45 @@ local mini = {
 			},
 		})
 		vim.keymap.set({ "n", "v" }, "<leader>ef", MiniFiles.open, { desc = "Explore Files" })
+
+		vim.keymap.set("n", "<leader>fc", function()
+			MiniFiles.open(vim.api.nvim_buf_get_name(0))
+		end, { desc = "Open mini.files (reveal current file)" })
+
+		local map_split = function(buf_id, lhs, direction)
+			local rhs = function()
+				-- Make new window and set it as target
+				local cur_target = MiniFiles.get_explorer_state().target_window
+				local new_target = vim.api.nvim_win_call(cur_target, function()
+					vim.cmd(direction .. " split")
+					return vim.api.nvim_get_current_win()
+				end)
+
+				MiniFiles.set_target_window(new_target)
+
+				-- This intentionally doesn't act on file under cursor in favor of
+				-- explicit "go in" action (`l` / `L`). To immediately open file,
+				-- add appropriate `MiniFiles.go_in()` call instead of this comment.
+				MiniFiles.go_in()
+			end
+
+			-- Adding `desc` will result into `show_help` entries
+			local desc = "Split " .. direction
+			vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+		end
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesBufferCreate",
+			callback = function(args)
+				local buf_id = args.data.buf_id
+				-- Tweak keys to your liking
+				map_split(buf_id, "<C-s>", "belowright horizontal")
+				map_split(buf_id, "<C-v>", "belowright vertical")
+				-- map_split(buf_id, "<C-t>", "tab")
+			end,
+		})
+
+		-------------------
 
 		require("mini.splitjoin").setup() -- `gS` to split or join lines
 	end,
